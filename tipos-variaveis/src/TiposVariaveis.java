bef3423b506ce1c256456b87b3031c708805f69f
Guto <guuto0044@gmail.com>
2024-05-15 10:11:13 -0300

    aula de variaveis completa

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,17 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+        
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+
+        int numero = 5;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+
+
+    }
+}
